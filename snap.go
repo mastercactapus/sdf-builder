11efdeff2fd745111f12cbf8a1c50dce643d149e
Nathaniel Caza <mastercactapus@gmail.com>
2018-02-24 15:14:20 -0600

    simplify snapping

--- snap.go
+++ snap.go
@@ -1,72 +1,64 @@
 package builder
 
-import "github.com/deadsy/sdfx/sdf"
+// SizeX returns the bounding box size for the X dimension.
+func (b Builder) SizeX() float64 { return b.BoundingBox().Size().X }
 
-// SnapX will cause the lower X edge of the receiver to match
-// the provided value.
-func (b Builder) SnapX(val float64) Builder {
-	return b.Translate(
-		val-b.BoundingBox().Min.X,
-		0,
-		0,
-	)
-}
+// SizeY returns the bounding box size for the Y dimension.
+func (b Builder) SizeY() float64 { return b.BoundingBox().Size().Y }
 
-// SnapY will cause the lower Y edge of the receiver to match
-// the provided value.
-func (b Builder) SnapY(val float64) Builder {
-	return b.Translate(
-		0,
-		val-b.BoundingBox().Min.Y,
-		0,
-	)
-}
+// SizeZ returns the bounding box size for the Z dimension.
+func (b Builder) SizeZ() float64 { return b.BoundingBox().Size().Z }
 
-// SnapZ will cause the lower Z edge of the receiver to match
-// the provided value.
-func (b Builder) SnapZ(val float64) Builder {
-	return b.Translate(
-		0,
-		0,
-		val-b.BoundingBox().Min.Z,
-	)
-}
+// MinX will return the low X-coordinate of the bounding box.
+func (b Builder) MinX() float64 { return b.BoundingBox().Min.X }
 
-// SnapXTo will cause the lower X edge of the receiver to match
-// the higher X edge of s. If s is nil, the X axis is used.
-func (b Builder) SnapXTo(s sdf.SDF3) Builder { return b.SnapXToOffset(s, 0) }
+// MidX will return the center X-coordinate of the bounding box.
+func (b Builder) MidX() float64 { return b.BoundingBox().Center().X }
 
-// SnapXToOffset will cause the lower X edge of the receiver to match
-// the higher X edge of s, plus an offset. If s is nil, the X axis is used.
-func (b Builder) SnapXToOffset(s sdf.SDF3, off float64) Builder {
-	if s == nil {
-		return b.SnapX(off)
-	}
-	return b.SnapX(s.BoundingBox().Max.X + off)
-}
+// MaxX will return the high X-coordinate of the bounding box.
+func (b Builder) MaxX() float64 { return b.BoundingBox().Max.X }
 
-// SnapYTo will cause the lower Y edge of the receiver to match
-// the higher Y edge of s. If s is nil, the Y axis is used.
-func (b Builder) SnapYTo(s sdf.SDF3) Builder { return b.SnapYToOffset(s, 0) }
+// SnapMinX will snap the low X-edge to the provided value.
+func (b Builder) SnapMinX(val float64) Builder { return b.Translate(val-b.MinX(), 0, 0) }
 
-// SnapYToOffset will cause the lower Y edge of the receiver to match
-// the higher Y edge of s, plus an offset. If s is nil, the Y axis is used.
-func (b Builder) SnapYToOffset(s sdf.SDF3, off float64) Builder {
-	if s == nil {
-		return b.SnapY(off)
-	}
-	return b.SnapY(s.BoundingBox().Max.Y + off)
-}
+// SnapMidX will snap the X-center to the provided value.
+func (b Builder) SnapMidX(val float64) Builder { return b.Translate(val-b.MidX(), 0, 0) }
 
-// SnapZTo will cause the lower Z edge of the receiver to match
-// the higher Z edge of s. If s is nil, the Z axis is used.
-func (b Builder) SnapZTo(s sdf.SDF3) Builder { return b.SnapZToOffset(s, 0) }
+// SnapMaxX will snap the high X-edge to the provided value.
+func (b Builder) SnapMaxX(val float64) Builder { return b.Translate(val-b.MaxX(), 0, 0) }
 
-// SnapZToOffset will cause the lower Z edge of the receiver to match
-// the higher Z edge of s, plus an offset. If s is nil, the Z axis is used.
-func (b Builder) SnapZToOffset(s sdf.SDF3, off float64) Builder {
-	if s == nil {
-		return b.SnapZ(off)
-	}
-	return b.SnapZ(s.BoundingBox().Max.Z + off)
-}
+// MinY will return the low Y-coordinate of the bounding box.
+func (b Builder) MinY() float64 { return b.BoundingBox().Min.Y }
+
+// MidY will return the center Y-coordinate of the bounding box.
+func (b Builder) MidY() float64 { return b.BoundingBox().Center().Y }
+
+// MaxY will return the high Y-coordinate of the bounding box.
+func (b Builder) MaxY() float64 { return b.BoundingBox().Max.Y }
+
+// SnapMinY will snap the low Y-edge to the provided value.
+func (b Builder) SnapMinY(val float64) Builder { return b.Translate(0, val-b.MinY(), 0) }
+
+// SnapMidY will snap the Y-center to the provided value.
+func (b Builder) SnapMidY(val float64) Builder { return b.Translate(0, val-b.MidY(), 0) }
+
+// SnapMaxY will snap the high Y-edge to the provided value.
+func (b Builder) SnapMaxY(val float64) Builder { return b.Translate(0, val-b.MaxY(), 0) }
+
+// MinZ will return the low Z-coordinate of the bounding box.
+func (b Builder) MinZ() float64 { return b.BoundingBox().Min.Z }
+
+// MidZ will return the center Z-coordinate of the bounding box.
+func (b Builder) MidZ() float64 { return b.BoundingBox().Center().Z }
+
+// MaxZ will return the high Z-coordinate of the bounding box.
+func (b Builder) MaxZ() float64 { return b.BoundingBox().Max.Z }
+
+// SnapMinZ will snap the low Z-edge to the provided value.
+func (b Builder) SnapMinZ(val float64) Builder { return b.Translate(0, 0, val-b.MinZ()) }
+
+// SnapMidZ will snap the Z-center to the provided value.
+func (b Builder) SnapMidZ(val float64) Builder { return b.Translate(0, 0, val-b.MidZ()) }
+
+// SnapMaxZ will snap the high Z-edge to the provided value.
+func (b Builder) SnapMaxZ(val float64) Builder { return b.Translate(0, 0, val-b.MaxZ()) }
