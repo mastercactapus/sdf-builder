e823e497855980a40344d259ef4d4cc9c2dec8d6
Nathaniel Caza <mastercactapus@gmail.com>
2018-03-03 11:16:16 -0600

    add rotate copy methods

--- builder.go
+++ builder.go
@@ -9,6 +9,12 @@ type Builder struct {
 	sdf.SDF3
 }
 
+// NewHexagon creates a hexagon with the given size (diameter) and height.
+func NewHexagon(size, height float64) Builder {
+	boxWidth := size / 1.75
+	return NewBox(size/2, boxWidth, height).SnapMinX(0).RotateZCopy(6)
+}
+
 // NewRTriangle creates a new right-triangle with the provided
 // opposite and adjacent lengths.
 func NewRTriangle(oLen, aLen, height float64) Builder {
@@ -26,6 +32,14 @@ func NewBox(x, y, z float64) Builder {
 	return Builder{SDF3: sdf.Box3D(sdf.V3{
 		X: x, Y: y, Z: z,
 	}, 0)}.Translate(0, 0, z/2)
+}
+
+// NewRoundedBox creates a new cubeoid with the given dimensions.
+// Edges will be rounded by the provided r value.
+func NewRoundedBox(x, y, z, r float64) Builder {
+	return Builder{SDF3: sdf.Box3D(sdf.V3{
+		X: x, Y: y, Z: z,
+	}, r)}.Translate(0, 0, z/2)
 }
 
 // NewCylinder creates a new cylinder shape with the provided
--- rotate.go
+++ rotate.go
@@ -1,11 +1,49 @@
 package builder
 
-import "github.com/deadsy/sdfx/sdf"
+import (
+	"math"
+
+	"github.com/deadsy/sdfx/sdf"
+)
+
+// RotateXCopy will create n copies, evenly rotated about the X axis.
+func (b Builder) RotateXCopy(n int) Builder {
+	return b.RotateXCopyOrigin(n, 0, 0, 0)
+}
+
+// RotateYCopy will create n copies, evenly rotated about the Y axis.
+func (b Builder) RotateYCopy(n int) Builder {
+	return b.RotateYCopyOrigin(n, 0, 0, 0)
+}
 
 // RotateZCopy will create n copies, evenly rotated about the Z axis.
 func (b Builder) RotateZCopy(n int) Builder {
-	b.SDF3 = sdf.RotateCopy3D(b.SDF3, n)
+	return b.RotateZCopyOrigin(n, 0, 0, 0)
+}
+
+func (b Builder) rotateCopyOrigin(fn func(float64, float64, float64, float64) Builder, n int, x, y, z float64) Builder {
+	copies := make([]sdf.SDF3, n)
+	for i := range copies {
+		copies[i] = fn(math.Pi*2/float64(n)*float64(i), x, y, z)
+	}
+
+	b.SDF3 = sdf.Union3D(copies...)
 	return b
+}
+
+// RotateXCopyOrigin will create n copies, evenly rotated using the coordinates as the origin.
+func (b Builder) RotateXCopyOrigin(n int, x, y, z float64) Builder {
+	return b.rotateCopyOrigin(b.RotateXOrigin, n, x, y, z)
+}
+
+// RotateYCopyOrigin will create n copies, evenly rotated using the coordinates as the origin.
+func (b Builder) RotateYCopyOrigin(n int, x, y, z float64) Builder {
+	return b.rotateCopyOrigin(b.RotateYOrigin, n, x, y, z)
+}
+
+// RotateZCopyOrigin will create n copies, evenly rotated using the coordinates as the origin.
+func (b Builder) RotateZCopyOrigin(n int, x, y, z float64) Builder {
+	return b.rotateCopyOrigin(b.RotateZOrigin, n, x, y, z)
 }
 
 // RotateX will rotate r radians about the X axis with origin (0, 0, 0).
@@ -26,32 +64,28 @@ func (b Builder) RotateZ(radians float64) Builder {
 	return b
 }
 
+func (b Builder) rotateOrigin(r sdf.M44, x, y, z float64) Builder {
+	p := sdf.V3{X: x, Y: y, Z: z}
+	b.SDF3 = sdf.Transform3D(b.SDF3, sdf.Translate3d(p.Negate()))
+	b.SDF3 = sdf.Transform3D(b.SDF3, r)
+	b.SDF3 = sdf.Transform3D(b.SDF3, sdf.Translate3d(p))
+	return b
+}
+
 // RotateXOrigin will rotate r radians about the X axis using the
 // provided coordinates as the origin.
 func (b Builder) RotateXOrigin(r, x, y, z float64) Builder {
-	p := sdf.V3{X: x, Y: y, Z: z}
-	b.SDF3 = sdf.Transform3D(b.SDF3, sdf.Translate3d(p.Negate()))
-	b.SDF3 = sdf.Transform3D(b.SDF3, sdf.RotateX(r))
-	b.SDF3 = sdf.Transform3D(b.SDF3, sdf.Translate3d(p))
-	return b
+	return b.rotateOrigin(sdf.RotateX(r), x, y, z)
 }
 
 // RotateYOrigin will rotate r radians about the Y axis using the
 // provided coordinates as the origin.
 func (b Builder) RotateYOrigin(r, x, y, z float64) Builder {
-	p := sdf.V3{X: x, Y: y, Z: z}
-	b.SDF3 = sdf.Transform3D(b.SDF3, sdf.Translate3d(p.Negate()))
-	b.SDF3 = sdf.Transform3D(b.SDF3, sdf.RotateY(r))
-	b.SDF3 = sdf.Transform3D(b.SDF3, sdf.Translate3d(p))
-	return b
+	return b.rotateOrigin(sdf.RotateY(r), x, y, z)
 }
 
 // RotateZOrigin will rotate r radians about the Z axis using the
 // provided coordinates as the origin.
 func (b Builder) RotateZOrigin(r, x, y, z float64) Builder {
-	p := sdf.V3{X: x, Y: y, Z: z}
-	b.SDF3 = sdf.Transform3D(b.SDF3, sdf.Translate3d(p.Negate()))
-	b.SDF3 = sdf.Transform3D(b.SDF3, sdf.RotateZ(r))
-	b.SDF3 = sdf.Transform3D(b.SDF3, sdf.Translate3d(p))
-	return b
+	return b.rotateOrigin(sdf.RotateZ(r), x, y, z)
 }
